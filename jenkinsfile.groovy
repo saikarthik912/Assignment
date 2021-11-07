cde1ecddc5dd9517f401c947c6014f527dcc835e
saikarthik912 <90164357+saikarthik912@users.noreply.github.com>
2021-11-07 14:57:37 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline{
+  agent any
+  stages{
+    stage('Git-Checkout'){
+      steps{
+        script{
+            cleanWs()
+           git url: 'https://github.com/saikarthik912/Assignment.git', branch: 'master'
+        }
+      }
+    }
+    stage('Build-and-Deploy'){
+      steps{
+        script{
+            sh "chmod -R 777 ."
+            echo "Build and Deploy"
+            sh "ls -la"
+            
+            sh "docker-compose down --rmi local --remove-orphans"
+            sh "docker-compose up --build -d"
+        }
+      }
+    }
+  }
+}
